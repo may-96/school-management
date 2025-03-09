154e5320560cc699c6c94c1d5ede70416eedf75a
20sixtn <umar007804@gmail.com>
2025-03-10 03:34:00 +0500

    login changes

--- routes/web.php
+++ routes/web.php
@@ -1,48 +1,20 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\LandingController;
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\StudentController;
-use App\Http\Controllers\TeacherController;
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\PaymentController;
+use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('welcome');
+});
 
-// landing page
-Route::get("/", [LandingController::class, "home"])->name("landing.home");
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-// auth
-Route::get("auth/forget", [AuthController::class, "forget"])->name("auth.forget");
-Route::get("auth/login", [AuthController::class, "login"])->name("auth.login");
-Route::get("auth/register", [AuthController::class, "register"])->name("auth.register");
-Route::get("auth/reset", [AuthController::class, "reset"])->name("auth.reset");
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-// mainpage
-Route::get("dashboard", [DashboardController::class, "show"])->name("school.dashboard");
-
-// teachers
-Route::get("teacher/create", [TeacherController::class, "create"])->name("teacher.create");
-Route::get("teacher/edit", [TeacherController::class, "edit"])->name("teacher.edit");
-Route::get("teacher/index", [TeacherController::class, "index"])->name("teacher.index");
-Route::get("teacher/profile", [TeacherController::class, "profile"])->name("teacher.profile");
-
-// students
-Route::get("student/create", [StudentController::class, "create"])->name("student.create");
-Route::get("student/edit", [StudentController::class, "edit"])->name("student.edit");
-Route::get("student/index", [StudentController::class, "index"])->name("student.index");
-Route::get("student/profile", [StudentController::class, "show"])->name("student.profile");
-
-// payments
-// Route::get("payments/create", [PaymentController::class, "create"])->name("payment.create");
-Route::get("payments/edit", [PaymentController::class, "edit"])->name("payment.edit");
-Route::get("payments/show", [PaymentController::class, "show"])->name("payment.show");
-Route::get("payments/list", [PaymentController::class, "list"])->name("payment.list");
-Route::get("payments/voucher", [PaymentController::class, "voucher"])->name("payment.voucher");
-
-
-// admin and social profile-User
-Route::get("admin/user", [ProfileController::class, "user"])->name("admin.user");
-Route::get("admin/users", [ProfileController::class, "profile"])->name("admin.users");
-
+require __DIR__.'/auth.php';
